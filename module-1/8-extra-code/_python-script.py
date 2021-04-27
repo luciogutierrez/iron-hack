078249e69558cb122985bea52d58e06b1f0447f5
Lucio_Gutierrez <luciogutierrez@users.noreply.github.com>
2021-04-27 12:59:21 -0500

    Extra-Code

---
+++ module-1/8-extra-code/_python-script.py
@@ -0,0 +1,10 @@
+n_odds = 0
+
+for i in range(1, 14, 2):
+    # check for the value of i in each iterations
+    breakpoint()
+    # bad condition
+    if i % 2 == 0:
+        n_odds += 1
+
+print(n_odds)
